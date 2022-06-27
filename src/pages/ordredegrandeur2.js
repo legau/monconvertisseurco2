2d77823b3e6555edcc7af68c5c8ab73c62f4de35
Florian <hi@florianpanchout.com>
2022-06-27 15:52:42 +0200

    Add page 2

---
+++ src/pages/ordredegrandeur2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Web from 'components/layout/Web'
+import Ranking2 from 'views/Ranking2'
+
+export default function ordredegrandeur() {
+  return (
+    <Web>
+      <Ranking2 />
+    </Web>
+  )
+}
